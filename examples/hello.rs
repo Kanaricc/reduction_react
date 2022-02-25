47055cf472287b9193b015dfd5d3a6f18aedaa8a
Kanaricc <iovo7c@gmail.com>
2022-02-25 06:55:01 +0000

    chores: 1

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,8 +1,10 @@
 use reduction_react::Reactor;
 
 fn main(){
-    let reactor=Reactor::new("hello","1.0.3","http://127.0.0.1:8080/hello.json");
+    let reactor=Reactor::new("hello","1.0.4","http://127.0.0.1:8080/hello.json");
     if let Err(e)=reactor.oneclick(){
         println!("failed to check updates: {}",e);
     }
+
+    println!("hello from 1.0.4");
 }
